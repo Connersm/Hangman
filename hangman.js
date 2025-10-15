e5949fe70b24eddb54068e3b4f8cdf2e537a7a0d
Connersm <143753907+Connersm@users.noreply.github.com>
2025-10-15 10:42:26 -0400

    Random Word

---
+++ hangman.js
@@ -0,0 +1,5 @@
+var POSSIBLE_WORDS = ["obdurate", "verisimlitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+
+var randomIdx = parseInt(Math.random() * (POSSIBLE_WORDS.length));
+
+var word = POSSIBLE_WORDS[randomIdx];
