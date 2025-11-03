f0dfcc80b0bdec7610b6a557de825764a1ec12b3
Connersm <143753907+Connersm@users.noreply.github.com>
2025-11-02 22:43:51 -0500

    added pictures

--- hangman.js
+++ hangman.js
@@ -1,12 +1,14 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimlitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 
 var word = "";
+var num = 0;
 
 function newGame() {
     var randomIdx = parseInt(Math.random() * (POSSIBLE_WORDS.length));
 
     word = POSSIBLE_WORDS[randomIdx];
     guesses = "";
+    num = 0;
 
     updatePage();
 }
@@ -20,8 +22,37 @@ function guessLetter(){
 }
 function updatePage(){
     var clueString = "";
+    if(word.indexOf(guesses[guesses.length - 1]) == -1) {
+        num ++;
+        pic = document.getElementById("pic");
+        switch(num) {
+            case(1):
+                pic.src = "img/hangman6.gif";
+                break;
+            case(2):
+                pic.src = "img/hangman5.gif";
+                break;
+            case(3):
+                pic.src = "img/hangman4.gif";
+                break;
+            case(4):
+                pic.src = "img/hangman3.gif";
+                break;
+            case(5):
+                pic.src = "img/hangman2.gif";
+                break;
+            case(6):
+                pic.src = "img/hangman1.gif";
+                break;
+            case(7):
+                pic.src = "img/hangman0.gif";
+                break;
+        }
+    }
+
     for(var i = 0; i < word.length; i++) {
         var currentLetter = word.charAt(i);
+
         if(guesses.indexOf(currentLetter) >= 0) {
             clueString += currentLetter + " ";
         } else {
