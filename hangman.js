1024ca3d1f85e5149aa60ea58d32e58e058e2dec
Connersm <143753907+Connersm@users.noreply.github.com>
2025-11-02 22:27:36 -0500

    base game

--- hangman.js
+++ hangman.js
@@ -1,5 +1,37 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimlitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 
-var randomIdx = parseInt(Math.random() * (POSSIBLE_WORDS.length));
+var word = "";
 
-var word = POSSIBLE_WORDS[randomIdx];
+function newGame() {
+    var randomIdx = parseInt(Math.random() * (POSSIBLE_WORDS.length));
+
+    word = POSSIBLE_WORDS[randomIdx];
+    guesses = "";
+
+    updatePage();
+}
+function guessLetter(){
+    var input = document.getElementById("guess");
+    var letter = input.value;
+
+    guesses += letter;
+
+    updatePage();
+}
+function updatePage(){
+    var clueString = "";
+    for(var i = 0; i < word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        } else {
+            clueString += "_ ";
+        }
+    }
+
+    var clue = document.getElementById("clue");
+    clue.innerHTML = clueString;
+
+    var guessesArea = document.getElementById("guesses");
+    guessesArea.innerHTML = "Guessed Letters: " + guesses;
+}
